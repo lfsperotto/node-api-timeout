99340587cebd7ee198986f1b5c213f76ba79596f
Luis Sperotto <luis.sperotto@ambevtech.com.br>
2021-07-27 18:20:03 +0000

    change port

--- index.js
+++ index.js
@@ -2,4 +2,17 @@ const express = require('express')
 
 const app = express()
 
-module.exports = app
+const port = process.env.PORT;
+
+const timeout = 2000;
+
+app.get('/', (req, res) => {
+    setTimeout(function() {
+        res.send('Time: ' + timeout + ' milisegundos');
+    }, timeout)
+  })
+
+app.listen(port, (err) => {
+    if (err) throw err
+    console.log('Server running in http://localhost:' + port + ' with timeout: ' + timeout + ' milisegundos')
+})
